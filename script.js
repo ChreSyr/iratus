3dda81f8ed6a1ddb5722365349fc15c9d98bed6a
ACER-SyméonRB <dev.chresyr@gmail.com>
2023-06-22 18:40:55 +0200

    Added alert for errors

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 
-const game = new Game(IratusBoard);
+try {
 
-setPiecesStyle();
+  const game = new Game(IratusBoard);
 
-// ATTACHING EVENT LISTENERS
-
-document.addEventListener("pointerdown", event => {
-
-  const boardDiv = document.getElementById("board-single");
-  if (boardDiv.contains(event.target) && boardDiv !== event.target) {return}
-
-  cancelPromotion();
-  let selectedPiece = game.board.selectedPiece;
-  if (selectedPiece) {
-    selectedPiece.unselect()
-  }
-});
-
-for (let promotionPiece of document.getElementsByClassName("promotion-piece")) {
-  promotionPiece.addEventListener("pointerdown", event => {
-    const color = game.board.pawnToPromote.color;
-    lastMove = game.movesHistory.slice(-1)[0];
-    lastMove.executeCommand(new Transform(game.board.pawnToPromote, Pawn, pieceClasses[promotionPiece.classList[1]]));
-    lastMove.notation += "=" + promotionPiece.classList[1].toUpperCase();
-    game.board.pawnToPromote = null;
-    game.board.updateAllValidMoves();
-    game.checkForEnd();
-
-    let promotionWindow = document.getElementsByClassName("promotion-window")[0];
-    promotionWindow.style.display = "none";
-    
-    let promotionPieces = document.getElementsByClassName("promotion-piece");
-    for (let promotionPiece of promotionPieces) {
-      promotionPiece.classList.remove(color + promotionPiece.classList[1]);
+  setPiecesStyle();
+  
+  // ATTACHING EVENT LISTENERS
+  
+  document.addEventListener("pointerdown", event => {
+  
+    const boardDiv = document.getElementById("board-single");
+    if (boardDiv.contains(event.target) && boardDiv !== event.target) {return}
+  
+    cancelPromotion();
+    let selectedPiece = game.board.selectedPiece;
+    if (selectedPiece) {
+      selectedPiece.unselect()
     }
   });
-}
+  
+  for (let promotionPiece of document.getElementsByClassName("promotion-piece")) {
+    promotionPiece.addEventListener("pointerdown", event => {
+      const color = game.board.pawnToPromote.color;
+      lastMove = game.movesHistory.slice(-1)[0];
+      lastMove.executeCommand(new Transform(game.board.pawnToPromote, Pawn, pieceClasses[promotionPiece.classList[1]]));
+      lastMove.notation += "=" + promotionPiece.classList[1].toUpperCase();
+      game.board.pawnToPromote = null;
+      game.board.updateAllValidMoves();
+      game.checkForEnd();
+  
+      let promotionWindow = document.getElementsByClassName("promotion-window")[0];
+      promotionWindow.style.display = "none";
+      
+      let promotionPieces = document.getElementsByClassName("promotion-piece");
+      for (let promotionPiece of promotionPieces) {
+        promotionPiece.classList.remove(color + promotionPiece.classList[1]);
+      }
+    });
+  }
+  
+  document.getElementsByClassName("promotion-cancel")[0].addEventListener("pointerdown", cancelPromotion);
 
-document.getElementsByClassName("promotion-cancel")[0].addEventListener("pointerdown", cancelPromotion);
+} catch (error) {
+  console.error(error);
+  alert(error);
+}
