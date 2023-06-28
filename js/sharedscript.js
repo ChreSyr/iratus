82f357d1feb948f8ffb2a662245e78989193399a
ACER-SyméonRB <dev.chresyr@gmail.com>
2023-06-28 19:27:37 +0200

    Added menu-wrapper

--- js/sharedscript.js
+++ js/sharedscript.js
@@ -1,4 +1,10 @@
 
+
+// Close the menu's bar on mobile view
+function closeMenu() {
+  document.getElementsByClassName("menu-bar-wrapper")[0].classList.add("closed-for-mobile")
+}
+
 // Handle when an option is selected in the style <select>
 function handleExperimentalSelect(type) {
   var selectedOption = document.getElementById("select-" + type).value;
@@ -17,4 +23,9 @@ function openKoFi() {
 // Open chess.com's learn page in a new web page
 function openLearnChess() {
   window.open('https://www.chess.com/fr/learn-how-to-play-chess', '_blank');
+}
+
+// Open the menu's bar on mobile view
+function openMenu() {
+  document.getElementsByClassName("menu-bar-wrapper")[0].classList.remove("closed-for-mobile")
 }
